c56ecaf0583ad11b50c242484f2086ce9d3e94f4
Shubhojeet Nayak <147873545+shubhojeet2107@users.noreply.github.com>
2026-02-03 22:36:12 +0530

    revision of JS

--- snippet.js
+++ snippet.js
@@ -1,9 +1,4 @@
-function getData(){
-    if([] == true){
-        console.log("Absent");
-    }else{
-        console.log("Present");
-    }
-}
+const arr = [1,2,3,4,5,6,7,8,9];
 
-getData();
+const ans2 = arr.reduce((acc, curr) => {return acc+curr}, 0);
+console.log(ans2);
